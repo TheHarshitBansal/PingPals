d6dadf00b61dd0c6e923fc94ff45336f4b5625c5
HARSHIT BANSAL <harshit.bansal1236@gmail.com>
2025-01-25 20:56:40 +0530

    Client Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-red-500">Hello</h1>
+      <Button variant="outline">Button</Button>
+    </>
+  );
+};
+
+export default App;
