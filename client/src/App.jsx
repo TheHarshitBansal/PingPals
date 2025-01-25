4042895b1f8dcf1103ce0c97f25c84ad00027882
HARSHIT BANSAL <harshit.bansal1236@gmail.com>
2025-01-25 23:31:31 +0530

    Added Sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "../src/layouts/Dashboard.jsx";
 
 const App = () => {
+  useEffect(() => {
+    const colorMode = JSON.parse(window.localStorage.getItem("colorMode"));
+    const className = "dark";
+
+    const bodyClass = window.document.body.classList;
+
+    colorMode === "dark"
+      ? bodyClass.add(className)
+      : bodyClass.remove(className);
+  }, []);
   return (
-    <>
-      <h1 className="text-red-500">Hello</h1>
-      <Button variant="outline">Button</Button>
-    </>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
   );
 };
 
---
+++ client/src/layouts/Dashboard.jsx
@@ -0,0 +1,77 @@
+import { Outlet } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import {
+  CogIcon,
+  MessageSquareMoreIcon,
+  PhoneIcon,
+  UserIcon,
+} from "lucide-react";
+import { Divider } from "@mui/material";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { faker } from "@faker-js/faker";
+import DarkModeSwitcher from "@/components/DarkModeSwitcher.jsx";
+import { useState } from "react";
+
+const Dashboard = () => {
+  const [active, setActive] = useState(1);
+
+  return (
+    <div>
+      <div
+        className={`h-screen w-[100px] bg-gray-100 dark:bg-gray-900 shadow-light dark:shadow-dark`}
+      >
+        <div className="h-full w-full flex flex-col items-center justify-between">
+          <div className="w-full flex flex-col items-center justify-center gap-y-3">
+            <div className="h-16 w-16 rounded-lg my-4 flex items-center justify-center">
+              <img src={Logo} alt="Logo" />
+            </div>
+            <div
+              className={`h-16 w-16 rounded-lg flex items-center cursor-pointer justify-center ${
+                active === 1 ? "bg-blue-500 text-white" : ""
+              }`}
+              onClick={() => setActive(1)}
+            >
+              <MessageSquareMoreIcon size={28} />
+            </div>
+            <div
+              className={`h-16 w-16 rounded-lg flex items-center cursor-pointer justify-center ${
+                active === 2 ? "bg-blue-500 text-white" : ""
+              }`}
+              onClick={() => setActive(2)}
+            >
+              <UserIcon size={28} />
+            </div>
+            <div
+              className={`h-16 w-16 rounded-lg flex items-center cursor-pointer justify-center ${
+                active === 3 ? "bg-blue-500 text-white" : ""
+              }`}
+              onClick={() => setActive(3)}
+            >
+              <PhoneIcon size={28} />
+            </div>
+            <Divider className="w-16" />
+            <div
+              className={`h-16 w-16 rounded-lg flex items-center cursor-pointer justify-center ${
+                active === 4 ? "bg-blue-500 text-white" : ""
+              }`}
+              onClick={() => setActive(4)}
+            >
+              <CogIcon size={28} />
+            </div>
+          </div>
+          <div className="w-full flex flex-col items-center justify-center mb-8 gap-y-3">
+            <div className="h-16 w-16 rounded-lg flex items-center justify-center">
+              <DarkModeSwitcher />
+            </div>
+            <Avatar>
+              <AvatarImage src={faker.image.avatar()} />
+              <AvatarFallback>User</AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+export default Dashboard;
