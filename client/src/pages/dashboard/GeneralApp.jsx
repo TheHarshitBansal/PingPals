746d574c1144993497e079a4706a2eb16a13e435
HARSHIT BANSAL <harshit.bansal1236@gmail.com>
2025-01-29 23:25:39 +0530

    Minor Fixes

--- client/src/pages/dashboard/GeneralApp.jsx
+++ client/src/pages/dashboard/GeneralApp.jsx
@@ -1,50 +1,24 @@
 import { useSelector } from "react-redux";
-import { lazy, Suspense } from "react";
-import LoadingSpinner from "../../components/LoadingSpinner.jsx";
-const ProfileSidebar = lazy(() => import("./ProfileSidebar.jsx"));
-const Chats = lazy(() => import("./Chats.jsx"));
-const MessageView = lazy(() => import("./MessageView.jsx"));
-const StarredSidebar = lazy(() => import("./StarredSidebar.jsx"));
-const SharedSidebar = lazy(() => import("./SharedSidebar.jsx"));
+import Chats from "./Chats.jsx";
+import MessageView from "./MessageView.jsx";
+import ProfileSidebar from "./ProfileSidebar.jsx";
+import StarredSidebar from "./StarredSidebar.jsx";
+import SharedSidebar from "./SharedSidebar.jsx";
 
 const GeneralApp = () => {
   const sidebar = useSelector((state) => state.app.sidebar);
   return (
     <div className="flex h-screen w-full">
       {/* Chats */}
-      <Suspense
-        fallback={
-          <div className="flex justify-center items-center h-screen w-full">
-            <LoadingSpinner />
-          </div>
-        }
-      >
-        <Chats />
-      </Suspense>
+      <Chats />
 
       {/* Chat Messages */}
-      <Suspense
-        fallback={
-          <div className="flex justify-center items-center h-screen w-4/5">
-            <LoadingSpinner />
-          </div>
-        }
-      >
-        <MessageView />
-      </Suspense>
+      <MessageView />
 
       {/* Chat Profile */}
-      <Suspense
-        fallback={
-          <div className="flex justify-center items-center h-screen w-1/4">
-            <LoadingSpinner />
-          </div>
-        }
-      >
-        {sidebar.isOpen && sidebar.type === "PROFILE" && <ProfileSidebar />}
-        {sidebar.isOpen && sidebar.type === "STARRED" && <StarredSidebar />}
-        {sidebar.isOpen && sidebar.type === "SHARED" && <SharedSidebar />}
-      </Suspense>
+      {sidebar.isOpen && sidebar.type === "PROFILE" && <ProfileSidebar />}
+      {sidebar.isOpen && sidebar.type === "STARRED" && <StarredSidebar />}
+      {sidebar.isOpen && sidebar.type === "SHARED" && <SharedSidebar />}
     </div>
   );
 };
