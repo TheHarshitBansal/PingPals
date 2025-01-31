4305b8b5943ff8fd089dd364426333efee814a23
HARSHIT BANSAL <harshit.bansal1236@gmail.com>
2025-02-01 02:54:16 +0530

    Basic Backend Setup

---
+++ server/app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import expressRateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors({
+    origin: '*',
+    credentials: true,
+    methods: 'GET, POST, PUT, DELETE, PATCH',
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(ExpressMongoSanitize());
+app.use(helmet());
+app.use(morgan('dev'));
+
+const limited = expressRateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 1000,
+    message: 'Too many requests, please try again later.'
+});
+
+app.use('/api', limited);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+export default app;
